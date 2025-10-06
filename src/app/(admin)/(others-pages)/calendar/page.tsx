5204e507cb6fbafc750f8f2b7d0197a7c89a0724
Maheer Akbar <maheerakbar@gmail.com>
2025-10-06 20:40:17 +0500

    build updated

--- src/app/(admin)/(others-pages)/calendar/page.tsx
+++ src/app/(admin)/(others-pages)/calendar/page.tsx
@@ -1,4 +1,4 @@
-import Calendar from "@/components/calendar/Calendar";
+// import Calendar from "@/components/calendar/Calendar";
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
 import { Metadata } from "next";
 import React from "react";
@@ -13,7 +13,7 @@ export default function page() {
   return (
     <div>
       <PageBreadcrumb pageTitle="Calendar" />
-      <Calendar />
+      {/* <Calendar /> */}
     </div>
   );
 }
