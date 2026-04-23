d6072a81a63653b1e6e6b76634f32e708a2d27db
Maheer Akbar <maheerakbar@gmail.com>
2026-04-24 01:45:43 +0500

    new build with script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Microsoft Clarity */}
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "wcnbu85lqt");
+            `,
+          }}
+        />
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-TZ0H8BREG0"
